cd6ba7b8e4855635010993e4b7c5ef84cb6ff801
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-24 21:46:40 +0530

    [AKASH]ADDED: Saving the Employee payroll object to the local storage

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -24,11 +24,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 const save = () => {
     try {
-        let empPayrollData = createEmployeePayroll();
-        console.log(empPayrollData);
+        let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     } catch (e) {
         return;
     }
+}
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if (employeePayrollList != undefined) {
+        employeePayrollList.push(employeePayrollData);
+    } else {
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
 const createEmployeePayroll = () => {
